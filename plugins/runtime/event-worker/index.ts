f2709deb2ce592a91d557a7b54706bdc9ecf092c
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2026-03-26 00:15:44 +0800

    add as to integartion tests (#47)

---
+++ plugins/runtime/event-worker/index.ts
@@ -0,0 +1,21 @@
+const eventManager = new globalThis.EventManager();
+
+console.log("event manager running");
+
+for await (const data of eventManager) {
+  if (data) {
+    switch (data.event_type) {
+      case "Log":
+        if (data.event.level === "Error") {
+          console.error(data.event.msg);
+        } else {
+          console.log(data.event.msg);
+        }
+        break;
+      default:
+        console.log(JSON.stringify(data, null, 2));
+    }
+  }
+}
+
+console.log("event manager exiting");
