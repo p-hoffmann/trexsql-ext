b147b3bbddb1a98e35b994dfdf016b74e2e7d3b2
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2026-05-04 13:55:20 +0800

    deno v2.7.12 upgrade (#128)

--- plugins/runtime/ext/trex/lib.rs
+++ plugins/runtime/ext/trex/lib.rs
@@ -995,7 +995,7 @@ impl Resource for RequestResource {
   }
 }
 
-#[op2(async)]
+#[op2]
 #[serde]
 async fn op_req(#[serde] message: JsonValue) -> Result<JsonValue, TrexError> {
   send_request_inner(message).await
@@ -1074,7 +1074,7 @@ fn op_req_listen(state: &mut OpState) -> Result<ResourceId, TrexError> {
   Ok(state.resource_table.add(resource))
 }
 
-#[op2(async)]
+#[op2]
 #[serde]
 async fn op_req_next(
   state: Rc<RefCell<OpState>>,
@@ -1229,7 +1229,7 @@ fn op_execute_query_stream(
 }
 
 #[allow(clippy::await_holding_lock)]
-#[op2(async)]
+#[op2]
 #[string]
 async fn op_execute_query_stream_next(
   state: Rc<RefCell<OpState>>,
